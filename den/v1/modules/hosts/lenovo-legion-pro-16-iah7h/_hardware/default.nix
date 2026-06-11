4e4189d809f95c69d6541c0fd279f5eb79037c6f
NTDuck <nguyentuduck@gmail.com>
2026-06-11 22:43:04 +0700

    add dummy hardware for lenovo host

--- den/v1/modules/hosts/lenovo-legion-pro-16-iah7h/_hardware/default.nix
+++ den/v1/modules/hosts/lenovo-legion-pro-16-iah7h/_hardware/default.nix
@@ -1,4 +1,7 @@
 {
-  imports = [
-  ];
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/00000000-0000-0000-0000-000000000000";
+    fsType = "ext4";
+  };
+  boot.loader.grub.device = "nodev";
 }
