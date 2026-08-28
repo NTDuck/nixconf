a60f270b232e1eb4ea952fb6eab85654eb4f97ee
NTDuck <nguyentuduck@gmail.com>
2026-08-28 22:16:21 +0700

    feat: bolt

---
+++ modules/hosts/lenovo-legion-16iah7h-PF3XJ8SP/private/firmware/bolt.nix
@@ -0,0 +1,11 @@
+{den, ...}: {
+  den.aspects.lenovo-legion-16iah7h-PF3XJ8SP = {
+    nixos = {pkgs, ...}: {
+      # https://wiki.nixos.org/wiki/Thunderbolt
+      services.hardware.bolt = {
+        enable = true;
+        package = pkgs.bolt;
+      }
+    };
+  };
+}
