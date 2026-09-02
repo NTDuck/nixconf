7928af0e457e0fa8cb440a8e88c18f68e78921f4
NTDuck <nguyentuduck@gmail.com>
2026-09-02 20:29:27 +0700

    fix: lmstudio memlock limits and ollama memory retention

--- modules/features/dev/agentics/lmstudio.nix
+++ modules/features/dev/agentics/lmstudio.nix
@@ -4,6 +4,24 @@
       environment.systemPackages = [
         pkgs.unstable.lmstudio
       ];
+
+      security.pam.loginLimits = [
+        {
+          domain = "@users";
+          item = "memlock";
+          type = "-";
+          value = "unlimited";
+        }
+        {
+          domain = "@wheel";
+          item = "memlock";
+          type = "-";
+          value = "unlimited";
+        }
+      ];
+
+      systemd.settings.Manager.DefaultLimitMEMLOCK = "infinity";
+      systemd.user.extraConfig = "DefaultLimitMEMLOCK=infinity";
     };
   };
 }
--- modules/hosts/lenovo-legion-16iah7h-PF3XJ8SP/private/dev/agentics/ollama.nix
+++ modules/hosts/lenovo-legion-16iah7h-PF3XJ8SP/private/dev/agentics/ollama.nix
@@ -34,6 +34,7 @@
         environmentVariables = {
           OLLAMA_FLASH_ATTENTION = "1";
           OLLAMA_KV_CACHE_TYPE = "q4_0";
+          OLLAMA_KEEP_ALIVE = "5m";
         };
       };
     };
