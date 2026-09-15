117c70e360557150f04c47267224ea13e129a32f
NTDuck <nguyentuduck@gmail.com>
2026-09-15 09:21:33 +0700

    feat: dbeaver

--- modules/features/dev/api/dbeaver.nix
+++ modules/features/dev/api/dbeaver.nix
@@ -1,11 +1,10 @@
 {den, ...}: {
   den.aspects.dev.api.dbeaver = {
-    nixos = {pkgs, ...}: {
-      environment.systemPackages = [
-        # `dbeaver` (source build) was dropped from nixpkgs; the prebuilt
-        # `dbeaver-bin` is the maintained derivation (26.2.0, asl20).
-        pkgs.unstable.dbeaver-bin
-      ];
+    homeManager = {pkgs, ...}: {
+      programs.dbeaver = {
+        enable = true;
+        package = pkgs.unstable.dbeaver-bin;
+      };
     };
   };
 }
